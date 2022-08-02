9103e5c12fb0c4d7707ed3fdf3b95a44df3742ee
unknown <dikshantsingh510@gmail.com>
2022-08-02 12:37:44 +0530

    adding code for heroku

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const hbs = require("hbs");
-const port = 3000;
+const port = process.env.PORT || 8000;
 const path = require("path");
 
 
